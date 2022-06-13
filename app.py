7096213f828f1f2c78f7bfa88d974b9c4267aee3
susana <susanaslc@gmail.com>
2022-06-12 21:46:21 -0300

    adicionando produtos ao carrinho e fazendo pagamento

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import traceback
 from chalice import Chalice
 
 from chalicelib import services
-from chalicelib.models import Pedido, Cliente
+from chalicelib.models import Pedido, Cliente, ItensCarrinho, Pagamento
 
 app = Chalice(app_name='projectbazar')
 
@@ -13,35 +13,48 @@ def index():
     return {'hello': 'world'}
 
 
-@app.route('/{cliente_id}/pedidos', methods=['POST'])
-def create_pedido(cliente_id):
+@app.route('/{cliente_id}/carrinho/{carrinho_id}', methods=['PUT'])
+def adicionar_carrinho(cliente_id, carrinho_id):
     try:
         request = app.current_request
         json_body = request.json_body
-        cliente = Cliente.get_by_id(cliente_id)
-        pagamento = services.save_pagamento(json_body['forma_pagamento'])
-        itens = json_body['itens']
-        pedido = Pedido(cliente=cliente_id, valor_total=0, pagamento=pagamento)
-        pedido.save()
-        valor_total = services.save_itens(itens, pedido.id)
-        valor_total = services.calcular_valor_total_frete(valor_total, cliente.endereco.estado)
-        pedido.valor_total = valor_total
-        pedido.replace()
-        return {"id-pedido": pedido.id}
+        item_carrinho = services.adicionar_carrinho(carrinho_id, json_body['item'])
+        return {'id_item': item_carrinho.id}
     except Exception as e:
         traceback.format_exc()
         raise e
 
 
-@app.route('/{cliente_id}/carrinho/frete', methods=['GET'])
-def simular_frete(cliente_id):
+@app.route('/{cliente_id}/carrinho/{carrinho_id}/frete', methods=['GET'])
+def simular_frete(cliente_id, carrinho_id):
     try:
-        request = app.current_request
-        json_body = request.json_body
         cliente = Cliente.get_by_id(cliente_id)
-        itens = json_body['itens']
+        itens = ItensCarrinho.select().where(ItensCarrinho.carrinho == carrinho_id)
         frete = services.simular_frete(itens, cliente.endereco.estado)
         return {'frete': frete}
     except Exception as e:
         traceback.format_exc()
         raise e
+
+
+@app.route('/{cliente_id}/carrinho/{carrinho_id}/fechar_pedido', methods=['POST'])
+def create_pedido(cliente_id, carrinho_id):
+    try:
+        request = app.current_request
+        json_body = request.json_body
+        pedido_id = services.criar_pedido(cliente_id, carrinho_id, json_body)
+        return {"id-pedido": pedido_id}
+    except Exception as e:
+        traceback.format_exc()
+        raise e
+
+
+@app.route('/pedidos/{id_pedido}/pagamento', methods=['POST'])
+def fazer_pagamento(id_pedido):
+    try:
+        status_pagamento = services.fazer_pagamento(id_pedido)
+        return {"status_pagamento": status_pagamento}
+    except Exception as e:
+        traceback.format_exc()
+        raise e
+
--- chalicelib/services.py
+++ chalicelib/services.py
@@ -1,4 +1,14 @@
-from chalicelib.models import ItensPedidos, Pedido, Produto, Pagamento
+from chalicelib.models import ItensPedidos, Pagamento, Carrinho, ItensCarrinho, Cliente, Pedido
+
+
+def adicionar_carrinho(carrinho_id, item):
+    Carrinho.get(id=carrinho_id)
+    item_carrinho = ItensCarrinho(carrinho=carrinho_id, produto=item['id_produto'],
+                                  quantidade=item['quantidade'], preco=item['preco'])
+    item_carrinho.save()
+    query = Carrinho.update(valor_total=Carrinho.valor_total + item['preco']).where(Carrinho.id == carrinho_id)
+    query.execute()
+    return item_carrinho
 
 
 def calcular_valor_total_frete(valor_total, estado):
@@ -22,19 +32,19 @@ def save_pagamento(forma_pagamento):
 def save_itens(itens, id_pedido):
     valor_total = 0
     for item in itens:
-        item_classe = ItensPedidos(produto=item['id_produto'],
+        item_classe = ItensPedidos(produto=item.produto,
                                    pedido=id_pedido,
-                                   quantidade=item['quantidade'],
-                                   preco=item['preco'])
+                                   quantidade=item.quantidade,
+                                   preco=item.preco)
         item_classe.save()
-        valor_total += item['preco']
+        valor_total += item.preco
     return valor_total
 
 
 def simular_frete(itens, estado):
     valor_total = 0
     for item in itens:
-        valor_total += item['preco']
+        valor_total += item.preco
     if valor_total > 100:
         return 0
     else:
@@ -44,3 +54,25 @@ def simular_frete(itens, estado):
             return 10
         else:
             return 20
+
+
+def criar_pedido(cliente_id, carrinho_id, json_body):
+    cliente = Cliente.get_by_id(cliente_id)
+    pagamento = save_pagamento(json_body['forma_pagamento'])
+    itens = ItensCarrinho.select().where(ItensCarrinho.carrinho == carrinho_id)
+    pedido = Pedido(cliente=cliente_id, valor_total=0, pagamento=pagamento)
+    pedido.save()
+    valor_total = save_itens(itens, pedido.id)
+    valor_total = calcular_valor_total_frete(valor_total, cliente.endereco.estado)
+    query = Pedido.update(valor_total=valor_total).where(Pedido.id == pedido.id)
+    query.execute()
+    return pedido.id
+
+
+def fazer_pagamento(id_pedido):
+    pedido = Pedido.get(Pedido.id == id_pedido)
+    pagamento_id = pedido
+    query = Pagamento.update(esta_pago=True).where(Pagamento.id == pagamento_id)
+    query.execute()
+    pagamento = Pagamento.get(Pagamento.id == pagamento_id)
+    return pagamento.esta_pago
